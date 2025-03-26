3aa42b92ddfd1321bfb8466b0ce7692002463df2
Tunz15 <eliteloner5@gmail.com>
2025-03-26 16:42:23 +0000

    Initial Commit

---
+++ src/main/java/com/backend/timeblock/TimeblockApplication.java
@@ -0,0 +1,13 @@
+package com.backend.timeblock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeblockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeblockApplication.class, args);
+	}
+
+}
